197fdf37a36f707fff89e8a79aad533f22576375
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-03-02 01:26:06 +0100

    Fix build system.

--- pkg/build.ml
+++ pkg/build.ml
@@ -3,8 +3,10 @@
 #use "topkg.ml";;
 
 let () =
-  Pkg.describe "result" ~builder:`OCamlbuild [
+  let builder = `Other ("ocamlbuild -classic-display", "_build") in
+  Pkg.describe "result" ~builder [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/result";
+    Pkg.lib ~exts:Exts.interface_opt "src/result_infix";
     Pkg.doc "README.md";
     Pkg.doc "CHANGES.md"; ]
