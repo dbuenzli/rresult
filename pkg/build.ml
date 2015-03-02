5f9ecce29aa87ac455580e0bfe385bf7715d4daa
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-03-03 00:48:35 +0100

    Toplevel support.

--- pkg/build.ml
+++ pkg/build.ml
@@ -3,10 +3,10 @@
 #use "topkg.ml";;
 
 let () =
-  let builder = `Other ("ocamlbuild -classic-display", "_build") in
-  Pkg.describe "result" ~builder [
+  Pkg.describe "result" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/result";
     Pkg.lib ~exts:Exts.interface_opt "src/result_infix";
+    Pkg.lib ~exts:Exts.library "src/result_top";
     Pkg.doc "README.md";
     Pkg.doc "CHANGES.md"; ]
